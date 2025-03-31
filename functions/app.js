d3bfe2630f3a92edb53b2b752e0e3bdb871a923b
GokilaSundar <gokithilaga267@gmail.com>
2025-03-31 12:18:53 +0530

    implement open ai  api configuration

--- functions/app.js
+++ functions/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import OpenAI from "openai";
 
 export const app = express();
 
@@ -8,8 +9,35 @@ dotenv.config();
 
 app.use(express.json());
 
+const client = new OpenAI({
+  baseURL: "https://api.aimlapi.com/v1",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 app.get("/api/hello", (req, res) => {
   res.send({ message: "Hello World!" });
+});
+
+// For list of available models check https://platform.openai.com/docs/models
+app.post("/api/conversation", async (req, res) => {
+  const messages = req.body;
+
+  if (!Array.isArray(messages) || !messages.length) {
+    return res.status(400).send({ message: "Messages cannot be empty!" });
+  }
+
+  try {
+    const response = await client.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages,
+    });
+
+    res.send(response.choices[0].message);
+  } catch (error) {
+    console.error("Failed to get response!", error);
+
+    res.status(500).send({ message: "Failed to get response!" });
+  }
 });
 
 if (process.env.MONGO_URI) {
